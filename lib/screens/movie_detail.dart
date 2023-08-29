61d98810068a4dfbbc915d3ccf6883eb84d130bd
petrichor-hl <tl.hoanglam174@gmail.com>
2023-08-29 21:50:28 +0700

    70% detail movie

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 import 'dart:math';
+import 'dart:ui' as dart_ui;
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:movie_app/assets.dart';
 import 'package:movie_app/main.dart';
 
 class MovieDeital extends StatefulWidget {
@@ -25,6 +28,7 @@ class _MovieDeitalState extends State<MovieDeital> {
       final Uri uri =
           Uri.https('api.themoviedb.org', '/3/movie/$randomMovieId', {
         'api_key': tmdbApiKey,
+        'append_to_response': 'credits',
         'language': 'vi-VN',
       });
       response = await http.get(uri);
@@ -46,6 +50,11 @@ class _MovieDeitalState extends State<MovieDeital> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Image.asset(
+          Assets.netflixLogo,
+          width: 140,
+        ),
+        centerTitle: true,
         backgroundColor: Colors.black,
         iconTheme: const IconThemeData(color: Colors.white),
         actions: [
@@ -77,85 +86,223 @@ class _MovieDeitalState extends State<MovieDeital> {
             text: TextSpan(
                 text: movie == null ? '' : movie!['overview'],
                 style: const TextStyle(color: Colors.white)),
-            maxLines: 2,
-            textDirection: TextDirection.ltr,
+            maxLines: 4,
+            textDirection: dart_ui.TextDirection.ltr,
           )..layout(minWidth: 0, maxWidth: MediaQuery.sizeOf(context).width);
 
           final isOverflowed = textPainter.didExceedMaxLines;
 
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network(
-                'https://image.tmdb.org/t/p/original/${movie!['backdrop_path']}',
-                height: 240,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-              Text(
-                movie!['title'],
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+          String genresText = '';
+          List<dynamic> genres = movie!['genres'];
+          if (genres.isNotEmpty) {
+            genresText =
+                (genres[0]['name'] as String).replaceFirst('Phim ', '');
+          }
+          for (int i = 1; i < genres.length; ++i) {
+            genresText +=
+                ' - ${(genres[i]['name'] as String).replaceFirst('Phim ', '')}';
+          }
+          final List<dynamic> cast = movie!['credits']['cast'];
+
+          DateTime releaseDate = DateTime.parse(movie!['release_date']);
+          String formattedDate = DateFormat('dd-MM-yyyy').format(releaseDate);
+
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  'https://image.tmdb.org/t/p/original/${movie!['backdrop_path']}',
+                  height: 240,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.play_arrow_rounded),
-                  label: const Text('Phát'),
-                  style: FilledButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
+                Text(
+                  movie!['title'],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.download_rounded),
-                  label: const Text('Phát'),
-                  style: FilledButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    backgroundColor: const Color.fromARGB(36, 255, 255, 255),
-                    foregroundColor: Colors.white,
+                // const SizedBox(height: 12),
+                Text(
+                  'Phát hành: $formattedDate',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              RichText(
-                text: TextSpan(
-                  text: movie!['overview'],
-                  style: GoogleFonts.montserrat().copyWith(color: Colors.white),
+                Text(
+                  'Điểm: ${(movie!['vote_average'] as double).toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                maxLines: _isExpandOverview ? null : 4,
-                textAlign: TextAlign.justify,
-              ),
-              if (isOverflowed)
-                InkWell(
-                  onTap: () => setState(() {
-                    _isExpandOverview = !_isExpandOverview;
-                  }),
-                  child: Text(
-                    _isExpandOverview ? 'Ẩn bớt' : 'Xem thêm',
+                const SizedBox(height: 12),
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.play_arrow_rounded),
+                    label: const Text(
+                      'Phát',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    style: FilledButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.download_rounded),
+                    label: const Text('Tải xuống'),
+                    style: FilledButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      backgroundColor: const Color.fromARGB(36, 255, 255, 255),
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: movie!['overview'],
+                    style:
+                        GoogleFonts.montserrat().copyWith(color: Colors.white),
+                  ),
+                  maxLines: _isExpandOverview ? null : 4,
+                  textAlign: TextAlign.justify,
+                ),
+                if (isOverflowed)
+                  InkWell(
+                    onTap: () => setState(() {
+                      _isExpandOverview = !_isExpandOverview;
+                    }),
+                    child: Text(
+                      _isExpandOverview ? 'Ẩn bớt' : 'Xem thêm',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                const SizedBox(
+                  height: 12,
+                ),
+                if (genres.isNotEmpty)
+                  const Text(
+                    'Thể loại:',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                if (genres.isNotEmpty)
+                  Text(
+                    genresText,
                     style: const TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                      color: Colors.white,
+                    ),
                   ),
-                )
-            ],
+                const SizedBox(
+                  height: 12,
+                ),
+                ActorsList(cast: cast),
+              ],
+            ),
           );
         },
       ),
+    );
+  }
+}
+
+class ActorsList extends StatelessWidget {
+  const ActorsList({
+    super.key,
+    required this.cast,
+  });
+
+  final List cast;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          'Diễn viên',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        SizedBox(
+          height: 170,
+          child: ListView.builder(
+            itemBuilder: (ctx, index) => cast[index]['profile_path'] != null
+                ? Stack(
+                    children: [
+                      Container(
+                        width: 130,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                'https://image.tmdb.org/t/p/original/${cast[index]['profile_path']}',
+                              ),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      Positioned(
+                        left: 10,
+                        right: 18,
+                        bottom: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.6),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+                          child: Text(
+                            cast[index]['original_name'],
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                : null,
+            itemCount: cast.length,
+            scrollDirection: Axis.horizontal,
+          ),
+        ),
+      ],
     );
   }
 }
